a914f0611a19479b8b0c3462781af3f22f9c4305
chris <orthagonal1@gmail.com>
2016-03-18 16:43:14 -0500

    no 'smart'  :(

--- README.md
+++ README.md
@@ -22,5 +22,3 @@ server.register({
 
 * 'append' -- detects incoming requests that have no trailing slash, adds one, and redirects to the new url
 * 'remove' -- detects incoming requests with a trailing slash, removes it, and redirects to the new url
-* 'smart' -- detects incoming requests that don't match any route, and checks if adding or removing a trailing slash will cause it to match, will internally
-          reroute the call if so
--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 'use strict';
 const _ = require('lodash');
 
-
-const ParamMatchingRegEx = new RegExp('(\\{)((?:[a-z][a-z0-9_]*))(\\*)(\\d+)(\\})', 'i');
-const replaceRouteParamWithValues = (route, paramName, paramValue) => {
-  // replace the most-common types of param:
-  route = route.replace('{' + paramName + '}', paramValue)
-  .replace('{' + paramName + '?}', paramValue)
-  .replace('{' + paramName + '*}', paramValue);
-  // match and replaces params of the form "{myParam*2}, {myParam*4}" as well:
-  const matchedValue = _.first(ParamMatchingRegEx.exec(route));
-  if (matchedValue) {
-    return route.replace(matchedValue, paramValue);
-  }
-  return route;
-};
-
 module.exports = (server, options, allDone) => {
   if (!options.method) {
     throw new Error('hapi-trailing-slash plugin registered without specifiying which method to use');
@@ -27,46 +12,16 @@ module.exports = (server, options, allDone) => {
     }
   };
 
-  const doInternalReroute = (path, originalRequest, originalReply) => {
-    logRedirect(originalRequest.path, path);
-    originalRequest.setUrl(path);
-    originalReply.continue();
-  };
-
   const doRedirect = (path, originalRequest, originalReply) => {
     let redirectTo = originalRequest.url.search ? path + originalRequest.url.search : path;
-    _.each(originalRequest.params, (param, paramName) => {
-      redirectTo = replaceRouteParamWithValues(redirectTo, paramName, param);
-    });
     logRedirect(originalRequest.path, redirectTo);
     return originalReply.redirect(redirectTo);
   };
 
-  if (options.method === 'smart') {
-    server.ext('onRequest', (request, reply) => {
-      // try to match the route with the path as-is:
-      const pre = server.match(request.method, request.path);
-      if (pre) {
-        return reply.continue();
-      }
-      // if there is no match, see if adding/removing a trailing slash fixes it:
-      let modifiedPath = '';
-      if (request.path[request.path.length - 1] === '/') {
-        modifiedPath = request.path.replace(/\/$/, '');
-      } else {
-        modifiedPath = request.path + '/';
-      }
-      const post = server.match(request.method, modifiedPath);
-      if (post) {
-        return doInternalReroute(modifiedPath, request, reply);
-      }
-      // if still no match just roll with it baby:
-      reply.continue();
-    });
-  } else if (options.method === 'append') {
+  if (options.method === 'append') {
     server.ext('onRequest', (request, reply) => {
       if (request.path[request.path.length - 1] !== '/') {
-        const slashedPath = request.path + '/';
+        const slashedPath = request.url.path + '/';
         return doRedirect(slashedPath, request, reply);
       }
       return reply.continue();
@@ -74,7 +29,7 @@ module.exports = (server, options, allDone) => {
   } else if (options.method === 'remove') {
     server.ext('onRequest', (request, reply) => {
       if (request.path[request.path.length - 1] === '/') {
-        const slashlessPath = request.path.replace(/\/$/, '');
+        const slashlessPath = request.url.path.replace(/\/$/, '');
         return doRedirect(slashlessPath, request, reply);
       }
       return reply.continue();
